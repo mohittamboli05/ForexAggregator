7c71d6087f82579d3e77f0d0b4c766087436e1bf
Sandeep Modi <sandeep.modi@valuelabs.com>
2020-11-21 14:45:21 +0530

    Updated UI

---
+++ ForexAggregator/ForexAggregator.UI/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using ForexAggregator.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ForexAggregator.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register(string returnUrl)
+        {
+            return View();
+        }
+    }
+}
