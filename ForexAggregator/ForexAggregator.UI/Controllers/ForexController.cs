f038691a7cb9a0f25f51a1956a817f140bfb1204
Sandeep Modi <sandeep.modi@valuelabs.com>
2020-11-22 12:33:08 +0530

    updated

--- ForexAggregator/ForexAggregator.UI/Controllers/ForexController.cs
+++ ForexAggregator/ForexAggregator.UI/Controllers/ForexController.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ForexAggregator.UI.Helper;
+using ForexAggregator.UI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace ForexAggregator.UI.Controllers
 {
     public class ForexController : Controller
     {
-        public IActionResult Index()
+        public IConfiguration Configuration { get; }
+        public ForexController(IConfiguration configuration)
         {
+            Configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await HttpClientHelper.GetAsync<ServiceResponse>(Configuration.GetSection("ApiBaseURL").Value + "/api/ForexAggregator/GetProviders", "");
             return View();
         }
     }
