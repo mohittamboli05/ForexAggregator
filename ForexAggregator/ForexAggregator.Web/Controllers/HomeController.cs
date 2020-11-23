6323ce7c3413cc0218d9c4f0acca7208dd9a52f3
Sandeep Modi <sandeep.modi@valuelabs.com>
2020-11-23 13:34:39 +0530

    updated history

--- ForexAggregator/ForexAggregator.Web/Controllers/HomeController.cs
+++ ForexAggregator/ForexAggregator.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using ForexAggregator.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace ForexAggregator.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         public IConfiguration Configuration { get; }
-        
+
         public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
@@ -46,6 +47,16 @@ namespace ForexAggregator.Web.Controllers
                 return PartialView(result.Data);
             }
             return PartialView(new List<Provider>());
+        }
+
+        public async Task<IActionResult> ProviderHistory(int providerId, string sourceCurrency, string targetCurrency)
+        {
+            ViewBag.ProviderId = providerId;
+            string request = string.Format("https://fcsapi.com/api-v3/forex/history?symbol={0}/{1}&period={2}&access_key=gpgjcVKG15ckcgn09QFtBCuOh", sourceCurrency, targetCurrency, "1d");
+            var provider = await HttpClientHelper.GetAsync<ServiceResponse<Provider>>(Configuration.GetSection("ApiBaseURL").Value + "ForexAggregator/GetProviderByProviderId?providerId=" + providerId, "");
+            var result = await HttpClientHelper.GetAsync<ExchangeRateHistory>(request, "");
+            result.Provider = provider.Data;
+            return View(result);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ ForexAggregator/ForexAggregator.Web/Models/ExchangeRateHistory.cs
@@ -0,0 +1,33 @@
+﻿namespace ForexAggregator.Web.Models
+{
+    public class ExchangeRateHistory
+    {
+        public bool status { get; set; }
+        public int code { get; set; }
+        public string msg { get; set; }
+        public Result[] response { get; set; }
+        public Info info { get; set; }
+        public Provider Provider { get; set; }
+    }
+
+    public class Info
+    {
+        public string id { get; set; }
+        public string _decimal { get; set; }
+        public string symbol { get; set; }
+        public string period { get; set; }
+        public string server_time { get; set; }
+        public int credit_count { get; set; }
+    }
+
+    public class Result
+    {
+        public string o { get; set; }
+        public string h { get; set; }
+        public string l { get; set; }
+        public string c { get; set; }
+        public string v { get; set; }
+        public int t { get; set; }
+        public string tm { get; set; }
+    }
+}
